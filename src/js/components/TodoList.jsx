041baa8766a6f1f560a5b9b247ecd2cc4e60d6d4
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-06-25 22:43:39 +0000

    variable rename, refactor one line

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -2,17 +2,15 @@ import React, { useState } from "react";
 
 export const TodoList = () => {
     const [tasks, setTasks] = useState([]);
-    const [input, setInput] = useState("");
+    const [input, setUserInput] = useState("");
 
     const addTask = () => {
         const task = input.trim();
         if (task) {
             setTasks([...tasks, task]);
-            setInput("");
+            setUserInput("");
         }
     };
-
-    const taskAddKey = (keyPressEvent) => keyPressEvent.key === "Enter" && addTask();
 
     const deleteTask = (index) => {
         setTasks(tasks.filter((_, i) => i !== index));
@@ -27,18 +25,18 @@ export const TodoList = () => {
                 className="form-control text-center"
                 placeholder="Type task here and press Enter"
                 value={input}
-                onChange={(newTaskEvent) => setInput(newTaskEvent.target.value)}
-                onKeyDown={taskAddKey}
+                onChange={(newTaskEvent) => setUserInput(newTaskEvent.target.value)}
+                onKeyDown={(keyPressEvent) => keyPressEvent.key === "Enter" && addTask()}
             />
 
             <ul className="list-group shadow mt-3 mb-2">
                 {tasks.length === 0 ? (
                     <li className="list-group-item text-muted text-center"> No pending tasks. You're all caught up! </li>
                 ) : (
-                    tasks.map((task, i) => (
-                        <li key={i} className="list-group-item d-flex justify-content-between">
+                    tasks.map((task, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between">
                             {task}
-                            <span className="text-danger fw-bold delete-btn" onClick={() => deleteTask(i)}> x </span>
+                            <span className="text-danger fw-bold delete-btn" onClick={() => deleteTask(index)}> x </span>
                         </li>
                     ))
                 )}
